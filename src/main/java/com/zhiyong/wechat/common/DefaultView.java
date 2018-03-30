1377b8f1d4bf48ef315c8d55d7f8b15c97e76f8b
zjxu <xzj_thrive@126.com>
2018-03-30 18:03:27 +0800

    Initial commit

---
+++ src/main/java/com/zhiyong/wechat/common/DefaultView.java
@@ -0,0 +1,15 @@
+package com.zhiyong.wechat.common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * @program: wechat
+ * @description: 默认主页
+ * @author: zjxu
+ * @create: 2018/3/30 17:44
+ **/
+@Configuration
+public class DefaultView extends WebMvcConfigurationSupport {
+
+}
